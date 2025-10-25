a1a9a07c2c179ab4242c1491813f5d966539699f
mit.levkin@vk.com <mit.levkin@vk.com>
2025-10-25 12:36:20 +0300

    created logic of the iteration

--- main.cpp
+++ main.cpp
@@ -4,5 +4,15 @@ using t = size_t;
 int main()
 {
     t cntr = 0;
-    
+    t a = 0;
+    std::cin >> a;
+    if (std::cin.eof())
+    {
+        std::cout << cntr << "\n";
+    }
+    else if (std::cin.fail())
+    {
+        std::cerr << "Error\n";
+        return 1;
+    }
 }
