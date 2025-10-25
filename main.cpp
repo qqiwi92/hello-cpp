0fe6de563fca40f09ec807cde022db55faf6cfe0
mit.levkin@vk.com <mit.levkin@vk.com>
2025-10-25 13:09:07 +0300

    created isPyth function

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 
 using t = size_t;
+
+bool isPyth(const t a, const t b, const t c)
+{
+    return (a * a == b * b + c * c) || (b * b == a * a + c * c) || (c * c == a * a + b * b);
+}
 int main()
 {
     t cntr = 0;
-    t current = 0;
+
+    t current = 0, b = 0, c = 0;
     while (std::cin >> current)
     {
-        cntr += current * current;
+        cntr += isPyth(c, b, current);
+        c = b;
+        b = current;
     }
     if (std::cin.eof())
     {
