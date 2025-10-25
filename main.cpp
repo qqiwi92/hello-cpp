722eb1dd4ceb297360f22785db36bf021efcad5c
mit.levkin@vk.com <mit.levkin@vk.com>
2025-10-25 12:42:13 +0300

    my version

--- main.cpp
+++ main.cpp
@@ -5,14 +5,22 @@ int main()
 {
     t cntr = 0;
     t a = 0;
-    std::cin >> a;
-    if (std::cin.eof())
+    while (true)
     {
-        std::cout << cntr << "\n";
-    }
-    else if (std::cin.fail())
-    {
-        std::cerr << "Error\n";
-        return 1;
+
+        std::cin >> a;
+        if (std::cin.eof())
+        {
+            std::cout << cntr << "\n";
+        }
+        else if (std::cin.fail())
+        {
+            std::cerr << "Error\n";
+            return 1;
+        }
+        else
+        {
+            cntr++;
+        }
     }
 }
